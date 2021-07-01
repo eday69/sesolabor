81f65464f1e7900e35e4ed85c2551fae24963379
Eric Day <eric.ch.day@gmail.com>
2021-06-30 22:37:55 -0600

    done

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,45 @@
 "use strict";
+var _ = require('lodash');
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+
+  const logEntry = (log) => new Promise((resolve, reject) => {
+    const value = log.popAsync();
+    resolve(value);
+  });
+
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+    const logEntries = logSources.map((log, i) => logEntry(log).then(
+      (res) => ({
+        log: res,
+        index: i
+      })
+    ));
+
+    Promise.all(logEntries)
+      .then((entries) => _.sortBy(entries, 'log.date'))
+      .then(async (sortedEntries) => {
+        let next = true;
+        do {
+          if (next) {
+            next = false;
+            const last = sortedEntries[0];
+            printer.print(last.log);
+            sortedEntries.splice(0, 1);
+            await logEntry(logSources[last.index]).then((newEntry) => {
+              if (newEntry) sortedEntries.push({log: newEntry, index: last.index});
+              sortedEntries = _.sortBy(sortedEntries, 'log.date');
+              next = true;
+            });
+          }
+        }
+        while(sortedEntries.length);
+      })
+      .then(() => {
+        printer.done();
+        resolve(console.log("Async sort complete."));
+      });
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,23 @@
 "use strict";
-
 // Print all entries, across all of the sources, in chronological order.
+var _ = require('lodash');
 
 module.exports = (logSources, printer) => {
+  let entries = logSources.map((log, i) => ({log: log.pop(), index: i}));
+  entries = _.sortBy(entries, 'log.date');
+
+  do {
+    const entry = entries.splice(0, 1);
+    printer.print(entry[0].log);
+
+    const newEntry = logSources[entry[0].index].pop();
+    if (newEntry) entries.push({ log: newEntry, index: entry[0].index });
+    entries = _.sortBy(entries, 'log.date');
+  }
+  while(entries.length);
+
+  printer.done();
   return console.log("Sync sort complete.");
 };
+
+
